377cfc917efefef7608a28c9ae8e20c5cd1b3df9
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-02-09 02:32:07 +0300

    Task 4 : Remove swagger annotation from DepremApiApplication

--- src/main/java/com/deprem/depremapi/DepremApiApplication.java
+++ src/main/java/com/deprem/depremapi/DepremApiApplication.java
@@ -2,10 +2,8 @@ package com.deprem.depremapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class DepremApiApplication {
 
     public static void main(String[] args) {
