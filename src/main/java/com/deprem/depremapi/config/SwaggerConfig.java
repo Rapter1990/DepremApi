366fcca76a4777443a315c9b2b7d250835930a9f
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-02-09 01:10:54 +0300

    Task 3 : Add debug logs into application.properties and Revise base package in SwaggerConfig

--- src/main/java/com/deprem/depremapi/config/SwaggerConfig.java
+++ src/main/java/com/deprem/depremapi/config/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.deprem"))
+                .apis(RequestHandlerSelectors.basePackage("com.deprem.depremapi"))
                 .paths(PathSelectors.regex("/.*"))
                 .build().apiInfo(apiEndPointsInfo());
 
