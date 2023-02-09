03c6697fc1a3e68a73328a69baea01062eb44dbd
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-02-09 10:27:08 +0300

    Task 8 : Fix the issue not open swagger and Add method's information for swagger

--- src/main/java/com/deprem/depremapi/api/EarthquakeController.java
+++ src/main/java/com/deprem/depremapi/api/EarthquakeController.java
@@ -3,6 +3,8 @@ package com.deprem.depremapi.api;
 import com.deprem.depremapi.model.EarthquakeAfad;
 import com.deprem.depremapi.model.EarthquakeKandilli;
 import com.deprem.depremapi.service.EarthQuakeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
@@ -15,10 +17,13 @@ import java.util.List;
 @RequestMapping("/api/v1/earthquakes")
 @RequiredArgsConstructor
 @Log4j2
+@Tag(description = "Earthquake APIs for Kandilli and Afad",
+        name = "Earthquake Controller")
 public class EarthquakeController {
 
     private final EarthQuakeService earthQuakesService;
 
+    @Operation(description = "Get All Earthquakes From Kandilli")
     @GetMapping("/kandili")
     public List<EarthquakeKandilli> getAllEarthquakesForKandilli() throws IOException, ParseException {
 
@@ -26,6 +31,7 @@ public class EarthquakeController {
         return earthQuakesService.getEarthquakesFromKandilli();
     }
 
+    @Operation(description = "Get All Earthquakes From Afad")
     @GetMapping("/afad")
     public List<EarthquakeAfad>  getAllEarthquakesForAfad() throws IOException, ParseException {
 
@@ -33,6 +39,7 @@ public class EarthquakeController {
         return earthQuakesService.getEarthQuakeDataFromAfad();
     }
 
+    @Operation(description = "Filter By Location From Kandilli Earthquakes")
     @GetMapping("/kandili/locations/{location}")
     public List<EarthquakeKandilli> filterByLocationForKandilli(@PathVariable String location) throws IOException, ParseException {
 
@@ -40,6 +47,7 @@ public class EarthquakeController {
         return earthQuakesService.filterByLocationForKandilli(location,earthQuakesService.getEarthquakesFromKandilli());
     }
 
+    @Operation(description = "Filter By Location From Afad Earthquakes")
     @GetMapping("/afad/locations/{location}")
     public List<EarthquakeAfad> filterByLocationForAfad(@PathVariable String location) throws IOException, ParseException {
 
@@ -47,6 +55,7 @@ public class EarthquakeController {
         return earthQuakesService.filterByLocationForAfad(location,earthQuakesService.getEarthQuakeDataFromAfad());
     }
 
+    @Operation(description = "Filter By Size ML From Kandilli Earthquakes")
     @GetMapping("/kandili/sizes/{size}")
     public List<EarthquakeKandilli> filterBySizeMLForKandilli(@PathVariable String size) throws IOException, ParseException {
 
@@ -54,6 +63,7 @@ public class EarthquakeController {
         return earthQuakesService.filterBySizeMLForKandilli(size,earthQuakesService.getEarthquakesFromKandilli());
     }
 
+    @Operation(description = "Filter By Size ML From Afad Earthquakes")
     @GetMapping("/afad/sizes/{size}")
     public List<EarthquakeAfad> filterBySizeMLForAfad(@PathVariable String size) throws IOException, ParseException{
 
@@ -61,6 +71,7 @@ public class EarthquakeController {
         return earthQuakesService.filterBySizeMLForAfad(size,earthQuakesService.getEarthQuakeDataFromAfad());
     }
 
+    @Operation(description = "Filter By Location and Size ML From Kandilli Earthquakes")
     @GetMapping("/kandili/search")
     public List<EarthquakeKandilli> filterByLocationAndSizeForKandilli(@RequestParam("location") String location,
                                                                        @RequestParam("size") String size) throws IOException, ParseException{
@@ -71,6 +82,7 @@ public class EarthquakeController {
         return earthQuakesService.filterByLocationAndSizeForKandilli(size,location,earthQuakesService.getEarthquakesFromKandilli());
     }
 
+    @Operation(description = "Filter By Location and Size ML From Afad Earthquakes")
     @GetMapping("/afad/search")
     public List<EarthquakeAfad> filterByLocationAndSizeForAfad(@RequestParam("location") String location,
                                                                    @RequestParam("size") String size
@@ -82,6 +94,7 @@ public class EarthquakeController {
         return earthQuakesService.filterByLocationAndSizeForAfad(size,location,earthQuakesService.getEarthQuakeDataFromAfad());
     }
 
+    @Operation(description = "Filter By Size Between Two ML's From Kandili Earthquakes")
     @GetMapping("/kandili/sizes")
     public List<EarthquakeKandilli> filterBySizeBetweenTwoMLForKandili(@RequestParam("minml") String minml,
                                                                        @RequestParam("maxml") String maxml) throws IOException, ParseException{
@@ -92,6 +105,7 @@ public class EarthquakeController {
         return earthQuakesService.filterBySizeBetweenTwoMLForKandili(minml,maxml,earthQuakesService.getEarthquakesFromKandilli());
     }
 
+    @Operation(description = "Filter By Size Between Two ML's From Afad Earthquakes")
     @GetMapping("/afad/sizes")
     public List<EarthquakeAfad> filterBySizeBetweenTwoMLForAfad(@RequestParam("minml") String minml,
                                                                 @RequestParam("maxml") String maxml) throws IOException, ParseException{
--- src/main/java/com/deprem/depremapi/config/OpenApiConfig.java
+++ src/main/java/com/deprem/depremapi/config/OpenApiConfig.java
@@ -1,11 +1,8 @@
 package com.deprem.depremapi.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
