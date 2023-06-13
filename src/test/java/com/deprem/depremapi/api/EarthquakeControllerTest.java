bcb8ea4927d70191e17c10ee5fdc0199edd6d967
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2023-06-14 00:53:57 +0300

    Task 18 : Revise EarthquakeControllerTest

--- src/test/java/com/deprem/depremapi/api/EarthquakeControllerTest.java
+++ src/test/java/com/deprem/depremapi/api/EarthquakeControllerTest.java
@@ -8,8 +8,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
